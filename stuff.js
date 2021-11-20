5f78aad4517183846e5e4fefc3283e7da3ef11de
unicorks <81965694+unicorks@users.noreply.github.com>
2021-11-20 20:39:38 +0530

    #3

--- stuff.js
+++ stuff.js
@@ -1,9 +1,12 @@
 window.addEventListener('load', function(){
+
     document.querySelector('#submit').addEventListener('click', () => {
         let input = document.querySelector('input');
-        if (input.value != '') {
+        if (input.value !== '') {
             document.querySelector('#hello').innerHTML = `Hello, ${input.value}. It's lovely to meet you!`
         } else {
             document.querySelector('#hello').innerHTML = "Hello, please enter your name!"
         }
-    })})
+    })
+
+})
